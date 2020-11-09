0b8ba5ff2a4fdeedbb1123c4399b56ddd68368a6
antoineroquis <54586678+antoineroquis@users.noreply.github.com>
2020-11-09 10:09:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 MISSING
 -------
 1)  Fonction de comparaison de résultat et de définition du gagnant, par round & à la partie (idée pour plus tard : affrontement au dé, calcul des résultats )
+2)  Formulaire de saisie des paramètres du jeu + bouton lancement du jeu
+3)  Front animé en Ajax
 
 
 IMPROVE
